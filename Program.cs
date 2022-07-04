13a558c93246bd53ac9cfbc4f107e90cefc10b5a
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-07-04 23:49:11 +0300

    Блок-схема и небольшие поправки в коде в части ввода новой переменной count

--- Program.cs
+++ Program.cs
@@ -4,15 +4,17 @@ string[] SearchAndFillData(string[] array)
 {
     int sizeNewArray = 0;
     int indexNewArray = 0;
-
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) sizeNewArray++;
+        count = array[i].Length;
+        if (count <= 3) sizeNewArray++;
     }
     string[] newArray = new string[sizeNewArray];
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3)
+        count = array[i].Length;
+        if (count <= 3)
         {
             newArray[indexNewArray] = array[i];
             indexNewArray++;
