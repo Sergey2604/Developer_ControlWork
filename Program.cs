ce05709e8a47f2d13ef29152a6c7f6563d702d5d
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-07-04 22:05:06 +0300

    Решение

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array = { "lkvz", "zrnl", "kvzkdl", "23", "5twta", "ki", "678" };//создание массива
+
+string[] SearchAndFillData(string[] array)
+{
+    int sizeNewArray = 0;
+    int indexNewArray = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) sizeNewArray++;
+    }
+    string[] newArray = new string[sizeNewArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[indexNewArray] = array[i];
+            indexNewArray++;
+        }
+    }
+    System.Console.WriteLine($"Старый массив - {string.Join(",", array)}");
+    System.Console.WriteLine($"Новый массив - {string.Join(",", newArray)}");
+    return newArray;
+}
+SearchAndFillData(array);
